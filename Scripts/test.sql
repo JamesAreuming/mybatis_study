f5043b546f08365661dca9a3f4377edd929c5a12
jamesareumin <hothihi5@gmail.com>
2020-03-30 12:19:30 +0900

    mybatis_study 초기화 작업완료 및 Connection, SqlSession 테스트 완료

---
+++ Scripts/test.sql
@@ -0,0 +1,7 @@
+select user(), database (); -- user_mybatis_study@localhost / mybatis_study
+
+select * from addresses;
+select * from tutors;
+select * from students;
+select * from course_enrollment;
+select * from courses;
